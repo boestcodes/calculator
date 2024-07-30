be318835f8de932f487fe9a001b45fe14363b16e
boestcodes <171556185+boestcodes@users.noreply.github.com>
2024-07-30 14:15:55 +0200

    Continue work on js

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 const buttons = document.querySelectorAll(".small, .big, .bigHorizontal");
+const display = document.querySelector("#displayBottom");
+const exec = document.querySelector("#equals");
+
+const firstNumber = 5;
+const secondNumber = 5;
+const operator = "+"
+
+display.textContent = `${firstNumber}${operator}${secondNumber}`
 
 buttons.forEach((btn) => {
     btn.addEventListener("click", () => { 
         console.log(btn.textContent);
+        
+
     });
 });
 
+exec.addEventListener("click", () => { 
+    operate(display.value);
+});
+
+function operate(content){
+    display.textContent = content;
+    console.log(content)
+    
+}
