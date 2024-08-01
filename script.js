677489417062080359cd70180cd655c65a798f8d
boestcodes <171556185+boestcodes@users.noreply.github.com>
2024-08-01 16:07:05 +0200

    Programm the logic for number input

--- script.js
+++ script.js
@@ -2,26 +2,53 @@ const buttons = document.querySelectorAll(".small, .big, .bigHorizontal");
 const display = document.querySelector("#displayBottom");
 const exec = document.querySelector("#equals");
 
-const firstNumber = 5;
-const secondNumber = 5;
-const operator = "+"
+let firstNumber;
+let secondNumber;
+let operator;
 
-display.textContent = `${firstNumber}${operator}${secondNumber}`
+const isNumerical = "0123456789"
+const isOperator = "+-*/"
+
+let tempButton = "";
+
+//display.textContent = `${firstNumber} ${operator} ${secondNumber}`
 
 buttons.forEach((btn) => {
-    btn.addEventListener("click", () => { 
-        console.log(btn.textContent);
-        
-
+    
+    btn.addEventListener("click", () => {
+        if(isNumerical.includes(btn.id)){
+         firstNumber += btn.id;
+         console.log(btn.id)
+         display.textContent = firstNumber;
+        }
     });
 });
 
+
 exec.addEventListener("click", () => { 
-    operate(display.value);
+    operate(display.textContent);
 });
 
 function operate(content){
-    display.textContent = content;
-    console.log(content)
+    let [Num1, op, Num2] = content.split(" ")
+    let value = 0;
+    Num1 = Number(Num1);
+    Num2 = Number(Num2);
+
+    switch (op){
+        case '+': value = Num1 + Num2;
+        break;
+
+        case '-': value = Num1 - Num2;
+        break;
+
+        case '*': value = Num1 * Num2;
+        break;
+
+        case '/': value = Num1 / Num2;
+        break;
+    }
+
+    display.textContent = value;
     
 }
